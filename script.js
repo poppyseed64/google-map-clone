2e8c84c10b376efc4faae5ceee2a29f9acb66a94
poppy <poppyseed6464@gmail.com>
2021-01-04 00:29:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicG9wcHlzZWVkNjQ2NCIsImEiOiJja2poZWFhZ2sxMHE5MnVudmJ0ejQyZXRoIn0.QQ5olS8pxE7ECjKddT--eQ';
+
+
+navigator.geolocation.getCurrentPosition(sucessLoacation,errorLocation,{enableHighAccuracy:true});
+function sucessLoacation(position){
+
+    console.log(position);
+    setupMap([position.coords.longitude,position.coords.latitude])
+}
+function errorLocation(){
+    setupMap([28.7041,77.1025])
+}
+
+function setupMap(center){
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center:center,
+        zoom:15
+    });
+     //control
+    map.addControl(new mapboxgl.NavigationControl());
+
+    //direction control 
+    var directions = new MapboxDirections({
+        accessToken: mapboxgl.accessToken,
+        
+      });
+     map.addControl(directions, 'top-left');
+
+      
+        // Add geolocate control to the map.
+    map.addControl(
+        new mapboxgl.GeolocateControl({
+        positionOptions: {
+        enableHighAccuracy: true
+        },
+        trackUserLocation: true
+        })
+        );
+   
+
+//         var popup = new mapboxgl.Popup({ closeOnClick: false })
+// .setLngLat(center)
+// .setHTML('<h1>start exploring</h1>')
+// .addTo(map);
+}
+   
